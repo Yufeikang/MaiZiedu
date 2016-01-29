f7be9a361896ba2cd90efa117730fd0a5f67062e
kang <kyf0722@gmail.com>
2016-01-29 22:17:41 +0800

    增加linux支持

--- MainText.py
+++ MainText.py
@@ -45,11 +45,20 @@ def main(root_dir=''):
         num += 1
         print "     %2d" % num + u".章节：" + section[1].decode('utf-8')
         print "        " + u"章节首页:" + section[0]
-    section_num = int(raw_input(u"输入要下载的章节编号：")) - 1
-    section_dir = course_dir + section_list[section_num][1] + DIR_SPLIT_C
-    section_dir = check_file_dir(section_dir)
-    downloader = DownloadSection(section_dir, section_list[section_num][0])
-    downloader.start_download_all()
+    section_num_list = raw_input(u"输入要下载的章节编号(下载多个用，分开，输入0下载所有)：")
+    section_num_list = section_num_list.split(',')
+    if '0' in section_num_list:
+        for _section in section_list:
+            section_dir = course_dir + _section[1] + DIR_SPLIT_C
+            section_dir = check_file_dir(section_dir)
+            downloader = DownloadSection(section_dir, _section[0])
+            downloader.start_download_all()
+    for section_num in section_num_list:
+        _section_num = int(section_num)-1
+        section_dir = course_dir + section_list[_section_num][1] + DIR_SPLIT_C
+        section_dir = check_file_dir(section_dir)
+        downloader = DownloadSection(section_dir, section_list[_section_num][0])
+        downloader.start_download_all()
 
 
 if __name__ == "__main__":
